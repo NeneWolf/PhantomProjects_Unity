72fab6d5034e48a99c45b58fffffd0e0854f8805
DimensionShift <jdole987@gmail.com>
2022-09-19 13:38:00 +0100

    Added Notes
    - Testing note interactions

--- Assets/Scripts/StoryScripts/DialogueManager.cs
+++ Assets/Scripts/StoryScripts/DialogueManager.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Queue<string> sentences;
+    public TextMeshProUGUI dialogueText;
+
+    private void Start()
     {
-        
+        sentences = new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartDialogue(Dialogue dialogue)
     {
-        
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        GameObject.Find("DialogueBox").SetActive(false);
+        Time.timeScale = 1f;
     }
 }
